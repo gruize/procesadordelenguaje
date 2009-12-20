072f6cffcc5c7601ff96c25b41476b052af1bf34
honguito42 <honguito42@e496d18e-d695-11de-8184-f791cb96e2ed>
2009-12-20 09:02:23 +0000

    Primeros pasos del codigo del interprete. Muy poco pero ya es algo.

---
+++ Codigo/plg/src/util/Memoria.java
@@ -0,0 +1,47 @@
+package util;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Memoria {
+	private Hashtable<Integer,Object> memoria;
+	
+	public Memoria(){
+		memoria=new Hashtable<Integer,Object>();
+	}
+	
+	public void setPosicion(int pos,Object obj){
+		memoria.put(pos,obj);
+	}
+	
+	public Object getPosicion(int pos){
+		return memoria.get(pos);
+	}
+	
+	public String toString(){
+		return memoria.toString();
+	}
+	
+	/**
+     * Esta funciona obtiene el indice del ultimo elemento
+	 * almacenado en la memoria.
+	 */
+	
+	public Integer getCima(){
+		Set<Integer> set = memoria.keySet();
+	    Iterator<Integer> itr = set.iterator();
+	    Integer inte;
+	    Integer max =0;
+	    while (itr.hasNext()) {
+	    	inte = itr.next();
+	    	if (inte > max )
+	    		max = inte;
+	    }
+	    return max;
+	}
+	
+	public int posicionMax(){
+		return 1000;
+	}
+}
